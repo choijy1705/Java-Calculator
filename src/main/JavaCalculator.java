fb09298ec8261febf9b918c9232d5e2df76092d7
Junyoung Choi <cjy1705@naver.com>
2020-12-02 09:42:39 +0900

    feat: 메인 및 UI 화면 생성

---
+++ src/main/JavaCalculator.java
@@ -0,0 +1,11 @@
+package main;
+
+public class JavaCalculator {
+
+    public static void main(String[] args){
+
+        UI uiCal = new UI();
+        uiCal.init();
+
+    }
+}
---
+++ src/main/UI.java
@@ -0,0 +1,15 @@
+package main;
+
+import javax.swing.*;
+
+public class UI {
+    private final JFrame frame;
+
+    private ImageIcon image;
+    private BufferedImageCustom imageReturn;
+
+    public UI(){
+        frame = new JFrame("Calculator");
+
+    }
+}
